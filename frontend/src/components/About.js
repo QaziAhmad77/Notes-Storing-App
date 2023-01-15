7c119f4f213f4c7ff2b516c10883965482628b65
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2023-01-15 17:32:07 +0500

    Update About.js

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const About = () => { 
-    return (
-        <div>
-            This is About page
-        </div>
-    )
-}
+const About = () => {
+  return <div>This is About</div>;
+};
 
-export default About
+export default About;
